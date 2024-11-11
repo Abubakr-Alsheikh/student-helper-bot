e54d8295f791a42441267dcafb411f82c98baf53
Mustafa Mordaa <mostafamordaa111@gmail.com>
2024-11-11 16:18:38 +0000

    adding docx generator and converter to pdf to traditional learning

--- main_menu_sections/traditional_learning/generating_materials.py
+++ main_menu_sections/traditional_learning/generating_materials.py
@@ -1,9 +1,62 @@
-
+from pathlib import Path
 from utils.database import get_data
+from template_maker.file_exports import convert_docx_to_pdf, generate_word_doc
+import os
+import asyncio
+import logging
 
-def generate_material_pdf(main_category_id, subcategory_id, question_id):
-     # Your PDF generation logic here.  Should return the path to the created PDF
-     #  For now, just return a dummy path for testing
+logger = logging.getLogger(__name__)
+
+async def generate_material_pdf(main_category_id, subcategory_id, question_id):
+    # Your PDF generation logic here.  Should return the path to the created PDF
+    #  For now, just return a dummy path for testing
+
+    template_path = (Path(__file__).parent / '..' / '..' / 'Main Files' / 'Template Files' /'Word' / 'Q&A old.docx').resolve()
+    docx_path = (Path(__file__).parent / '..' / '..' / 'temp' / f'{question_id}.docx').resolve()
+    pdf_path = (Path(__file__).parent / '..' / '..' / 'temp' / f'{question_id}.pdf').resolve()
+    
+    try:
+        question_data = get_data(
+            """
+            SELECT question_text, option_a, option_b, option_c, option_d, correct_answer, explanation
+            FROM questions
+            WHERE id = ?
+            """,
+            (question_id,),
+        )[0]  # Extract the first tuple
+
+        (
+            question_text,
+            option_a,
+            option_b,
+            option_c,
+            option_d,
+            correct_answer,
+            explanation,
+        ) = question_data
+
+        print(question_data)
+        
+        try:
+            await generate_word_doc(template_path, docx_path, question_data)
+        except Exception as e:
+            logger.error(f"Error generating Word doc: {e}")
+            return None
+        try:
+            await convert_docx_to_pdf(docx_path, pdf_path)
+        except Exception as e:
+            logger.error(f"Error converting to PDF: {e}")
+            return None
+
+        # 5. Cleanup the temporary Word file (optional)
+        # if os.path.exists(docx_path):
+        #     os.remove(docx_path)
+
+        return pdf_path
+    except Exception as e:
+        logger.error(f"An unexpected error occurred in generate_quiz_pdf: {e}")
+        return None
+    
     return None
 
 
@@ -54,3 +107,5 @@ def generate_material_text(main_category_id, subcategory_id, question_id):
 {explanation}
 """
     return plain_text
+
+asyncio.run(generate_material_pdf(55, 34, 11))
