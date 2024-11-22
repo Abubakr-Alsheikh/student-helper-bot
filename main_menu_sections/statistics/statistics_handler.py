cc11a187cd223b756bf0ea32860e75ada3ce7c37
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-11-22 11:13:27 +0300

    Fixing SyntaxError

--- main_menu_sections/statistics/statistics_handler.py
+++ main_menu_sections/statistics/statistics_handler.py
@@ -317,7 +317,7 @@ async def handle_progress_graph(update: Update, context: CallbackContext):
         z = np.polyfit(mdates.date2num(formatted_dates), percentages, 1)
         p = np.poly1d(z)
         ax_level_det.plot(
-            formatted_dates, p(mdates.date2num(formatted_dates)), "r--", label=get_display(arabic_reshaper.reshape("التحسن")
+            formatted_dates, p(mdates.date2num(formatted_dates)), "r--", label=get_display(arabic_reshaper.reshape("التحسن"))
         )
         ax_level_det.legend()
 
